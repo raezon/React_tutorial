7dd5640f6f8c74f4ec507938dfa1c047cca538ec
sayen akira <amardjebabla10@gmail.com>
2020-10-10 23:10:36 +0100

    Lecteur 19 form1

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -5,19 +5,30 @@ import Items from './components/items.js'
 
 class  App extends Component {
   state={
-    items:[
-        {id:1,name:'ahmed',age:22},
-        {id:2,name:'tarek',age:25},
-        
-    ]
-}
+    name:''
+  }
+  handleChange=(e)=>{
+    this.setState({
+      name:e.target.value
+    })
+  }
+  handleSubmit=(e)=>{
+    //stoper le rafrichissemnt de la page dans un onclick
+    e.preventDefault();
+    console.log(this.state.name)
+  }
 
   render(){
  
     return (
       <div className="App">
-        <Items items={this.state.items}/>
-        {Math.random()*10}
+        Form Part1
+        <form onSubmit={this.handleSubmit}>
+          <textarea type="text"  onChange={this.handleChange}/>
+          <input type="submit"  value="send"/>
+        
+        </form>
+        {this.state.name}
 
       </div>
     );
